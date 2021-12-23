92b1700d9181abb50d12143b7d89659b8703b327
Enzo D <enzo.danjour@gmail.com>
2021-12-23 17:09:03 -0300

    create splash page

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -7,8 +7,17 @@ class SplashPage extends StatelessWidget {
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
+           appBar: AppBar(title: const Text('/splash'),),
+           body: Center(
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: [
+                 ElevatedButton(onPressed: (){
+                   Navigator.pushNamed(context, '/login');
+                 }, child: Text('/login'))
+               ],
+             ),
+           ),
        );
   }
 }
